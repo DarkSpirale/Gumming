b7f57b766e0df09b36ab5786dd15abb8cb96c32b
DarkSpirale <71567838+DarkSpirale@users.noreply.github.com>
2022-08-24 21:46:42 +0200

    Generic Methods

--- Assets/Scripts/FileSystem/SaveLevel.cs
+++ Assets/Scripts/FileSystem/SaveLevel.cs
@@ -11,7 +11,9 @@ public class SaveLevel : MonoBehaviour
     [SerializeField] private Transform gummingsRef;
     [SerializeField] private Transform exitsRef;
 
-    public List<Transform> props = new List<Transform>();
+    private List<Transform> props = new List<Transform>();
+    private List<Transform> gummings = new List<Transform>();
+    private List<Transform> exits = new List<Transform>();
 
     private SerializableDataToSave serializedDatas = new SerializableDataToSave();
 
@@ -39,6 +41,8 @@ public class SaveLevel : MonoBehaviour
     private void GetChildrenFromGO()
     {
         props = GetChildren(propsRef);
+        gummings = GetChildren(gummingsRef);
+        exits = GetChildren(exitsRef);
     }
 
     private List<Transform> GetChildren(Transform goRef)
@@ -54,43 +58,42 @@ public class SaveLevel : MonoBehaviour
 
     private void SaveToJson()
     {
-        serializedDatas.propsDataList = ConvertGOListToEditorDataList<PropsData>(props);
+        SerializeDatas();        
 
         var stringJS = JsonUtility.ToJson(serializedDatas);
-
-        /*var propsJS = new System.Text.StringBuilder();
-
-        foreach(PropsData prop in propsDataList)
-        {
-            propsJS.AppendLine(JsonUtility.ToJson(prop));
-        }*/
-
         string filePath = Application.persistentDataPath + "/" + levelName + ".json";
-        Debug.Log(filePath);
+        //Debug.Log(filePath);
 
         System.IO.File.WriteAllText(filePath, stringJS);
     }
 
-    private List<T> ConvertGOListToEditorDataList<T>(List<Transform> inputList) where T: EditorData
+    private void SerializeDatas()
+    {
+        serializedDatas.propsDataList = ConvertGOListToEditorDataList<PropsData>(props);
+        serializedDatas.gummingsDataList = ConvertGOListToEditorDataList<GummingsData>(gummings);
+        serializedDatas.exitsDataList = ConvertGOListToEditorDataList<EditorData>(exits);
+    }
+
+    private List<T> ConvertGOListToEditorDataList<T>(List<Transform> inputList) where T: EditorData, new()
     {
         List<T> targetList = new List<T>();
 
         foreach (Transform go in inputList)
         {
-            //T targetObject = typeof(T).GetMethod("ConvertGOToEditorData").Invoke(null, new object[] { go }) as T;
-            //targetList.Add(targetObject);
-            targetList.Add(EditorData.ConvertGOToEditorData<T>(go));
+            T targetObject = EditorData.ConvertGOToEditorData<T>(go);
+            targetList.Add(targetObject);
         }
 
         return targetList;
     }
-    
 }
 
 [System.Serializable]
 public struct SerializableDataToSave
 {
     public List<PropsData> propsDataList;
+    public List<GummingsData> gummingsDataList;
+    public List<EditorData> exitsDataList;
 }
 
 
--- Assets/Scripts/Structs/EditorStructs.cs
+++ Assets/Scripts/Structs/EditorStructs.cs
@@ -5,14 +5,18 @@ public class EditorData
 {
     public Vector3 position;
 
-    public EditorData(Vector3 position)
+    public EditorData() { }
+
+    public static T ConvertGOToEditorData<T>(Transform go) where T: EditorData, new()
     {
-        this.position = position;
+        T newEditorData = new T();
+        newEditorData.Initialize(go);
+        return newEditorData;
     }
 
-    public static EditorData ConvertGOToEditorData(Transform go)
+    protected virtual void Initialize(Transform go)
     {
-        return new EditorData(go.position);
+        position = go.position;
     }
 }
 
@@ -23,19 +27,15 @@ public class PropsData : EditorData
     public Quaternion rotation;
     public Vector3 scale;
 
-    public PropsData(Vector3 position, Quaternion rotation, Vector3 scale, int id) : base(position)
-    {
-        this.id = id;
-        this.rotation = rotation;
-        this.scale = scale;
-    }
+    public PropsData() { }
 
-    public static new PropsData ConvertGOToEditorData(Transform go)
+    protected override void Initialize(Transform go)
     {
-        //TODO: getID
-        int id = 1;
+        base.Initialize(go);
 
-        return new PropsData(go.position, go.rotation, go.localScale, id);
+        id = 1;
+        rotation = go.rotation;
+        scale = go.localScale;
     }
 }
 
@@ -44,15 +44,14 @@ public class GummingsData : EditorData
 {
     public int facingDirection = 1;
 
-    public GummingsData(Vector3 position, int facingDirection) : base(position)
-    {
-        this.facingDirection = facingDirection;
-    }
+    public GummingsData() { }
 
-    public static new GummingsData ConvertGOToEditorData(Transform go)
+    protected override void Initialize(Transform go)
     {
+        base.Initialize(go);
+
         GummingEditor gumming = go.GetComponent<GummingEditor>();
 
-        return new GummingsData(go.position, gumming.FacingDirection);
+        facingDirection = gumming.FacingDirection;
     }
 }
